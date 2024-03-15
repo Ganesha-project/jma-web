68f965aa8e15fe383b463e78659ecce95a7a9b61
preettypeanuts <mrafly212001@gmail.com>
2024-03-15 20:18:25 +0700

    change phone number

--- src/components/FloatingButon.jsx
+++ src/components/FloatingButon.jsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 import { RiWhatsappFill } from 'react-icons/ri';
 
 export const FloatingButton = () => {
-    const WaLink = "https://api.whatsapp.com/send?phone=628887127000&text=Hello%20JMA%20Indonesia"
+    const WaLink = "https://api.whatsapp.com/send?phone=082188260598&text=Hello%20JMA%20Indonesia"
     const [isVisible, setIsVisible] = useState(false);
 
     useEffect(() => {
